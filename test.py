31b740181740286f2d56892d1c697d4e90819483
alexseysoldatenko <126325350+alexseysoldatenko@users.noreply.github.com>
2023-10-14 15:30:20 +0300

    first_seminar

---
+++ test.py
@@ -0,0 +1,92 @@
+import gym
+import gym_maze
+import numpy as np
+import time
+
+env = gym.make('maze-sample-5x5-v0')
+state_n = 25
+action_n = 4
+
+class RandomAgent():
+    def __init__(self, action_n):
+        self.action_n = action_n
+
+    def get_action(self, state):
+        action = np.random.randint(self.action_n)
+        return action
+    
+class CrossEntropyAgent():
+    def __init__(self, state_n, action_n):
+        self.state_n = state_n
+        self.action_n = action_n
+        self.model = np.ones((state_n,action_n)) / action_n
+
+    def get_action(self, state):
+        action = np.random.choice(np.arange(self.action_n), p=self.model[state])
+        return int(action)
+    
+    def fit(self, elite_trajectories):
+        new_model = np.zeros_like(self.model)
+        for trajectory in elite_trajectories:
+            for state, action in zip(trajectory['states'], trajectory['actions']):
+                new_model[state][action] += 1
+
+        for state in range(self.state_n):
+            if np.sum(new_model[state]) == 0:
+                new_model[state] = self.model[state].copy()
+            else:
+                new_model[state] /= np.sum(new_model[state])
+
+        self.model = new_model
+        
+
+def get_state(obs):
+     return int(np.sqrt(state_n) * obs[0] + obs[1])
+
+def get_trajectory(env, agent, max_len=1000, visualize=False):
+    trajectory = {'states': [], 'actions': [], 'rewards': []}
+    obs = env.reset()
+    state = get_state(obs)
+    for _ in range(1000):
+        trajectory['states'].append(state)
+        
+        action = agent.get_action(state)
+        trajectory['actions'].append(action)
+
+        obs, reward, done, _ = env.step(action)
+        trajectory['rewards'].append(reward)
+        state = get_state(obs)
+        
+        if visualize:
+            env.render()
+            time.sleep(0.1)
+
+        if done:
+            break
+    return trajectory
+
+agent = CrossEntropyAgent(state_n, action_n)
+q_param = 0.9
+trajectories_n = 50
+iteration_n = 20
+
+for iteration in range(iteration_n):
+
+    # policy evaluation
+    trajectories = [get_trajectory(env, agent) for _ in range(trajectories_n)]
+    total_reward = [np.sum(trajectory['rewards']) for trajectory in trajectories]
+    print('iteration:', iteration, 'mean total reward:', np.mean(total_reward))
+    
+    # policy improvement
+    quantile = np.quantile(total_reward, q_param)
+    elite_trajectories = []
+    for trajectory in trajectories:
+        if np.sum(trajectory['rewards']) > quantile:
+            elite_trajectories.append(trajectory)
+    agent.fit(elite_trajectories)
+
+trajectory = get_trajectory(env, agent, visualize=True)
+
+print('total reward:', sum(trajectory['rewards']))
+print('model:')
+print(agent.model)
